7f975940696dfe11075505c548e4a69656c781d9
gabriannad <gabrianna@berkeley.edu>
2015-01-03 12:47:05 -0800

    tabs fixed ii

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 (function(){
 	var app = angular.module('store', [ ]);
 
-
 	app.controller('StoreController', function(){
 		this.products = gems;
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
 	});
 
 	var gems = [
@@ -11,6 +20,16 @@
 			name: 'Dodecahedron',
 			price: 2.95,
 			description: '. . .',
+			images: [
+				{
+					full: 'dodecahedron-01-full.jpg',
+					thumb: 'dodecahedron-01-thumb.jpg'
+				},
+				{
+					full: 'dodecahedron-02-full.jpg',
+					thumb: 'dodecahedron-02-thumb.jpg'
+				}
+			]
 			canPurchase: true,
 			soldOut: false,
 		},
